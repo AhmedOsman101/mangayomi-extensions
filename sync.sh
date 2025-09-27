6d62e8eeb05cc97cf36a554ba1d93f33d585dfe0
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-09-27 23:59:11 +0300

    Updated Files 2025-09-27@11:59PM

--- sync.sh
+++ sync.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
-trap 'exit 1' SIGUSR1
-
-# ---  Main script logic --- #
-cp -f index.json manga/index.min.json
-cp -f novel_index.json novel/index.min.json
+mkdir -p {manga,anime,novel} || true
+cp -f index.json manga/index.min.json || true
+cp -f novel_index.json novel/index.min.json || true
+cp -f anime_index.json anime/index.min.json || true
